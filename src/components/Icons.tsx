5c23d257b710f83a1fbd4170b2c5b7c8544415e0
Akshat Singh <akshatsinghdelhi@gmail.com>
2023-08-02 17:03:45 +0530

    basic setup and navbar

---
+++ src/components/Icons.tsx
@@ -0,0 +1,42 @@
+import { LucideProps } from 'lucide-react';
+
+export const Icons = {
+    logo: (props: LucideProps) => (
+        <svg
+            {...props}
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <g clipPath="url(#clip0_1_2)">
+                <path
+                    d="M12 16C14.2091 16 16 14.2091 16 12C16 9.79086 14.2091 8 12 8C9.79086 8 8 9.79086 8 12C8 14.2091 9.79086 16 12 16Z"
+                    stroke="black"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                />
+                <path
+                    d="M16 8V13C16 13.7957 16.3161 14.5587 16.8787 15.1213C17.4413 15.6839 18.2043 16 19 16C19.7956 16 20.5587 15.6839 21.1213 15.1213C21.6839 14.5587 22 13.7957 22 13V12C22 9.74731 21.2394 7.5606 19.8414 5.79417C18.4434 4.02774 16.49 2.78507 14.2975 2.26751C12.1051 1.74995 9.80213 1.98781 7.76176 2.94255C5.7214 3.8973 4.06316 5.51299 3.05572 7.52786C2.04829 9.54274 1.75067 11.8387 2.2111 14.0439C2.67152 16.249 3.86301 18.2341 5.59252 19.6775C7.32203 21.1209 9.48824 21.9381 11.7402 21.9966C13.9921 22.0552 16.1978 21.3516 18 20"
+                    stroke="black"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                />
+                <rect
+                    x="23.6621"
+                    y="-1"
+                    width="2"
+                    height="34.8775"
+                    transform="rotate(45 23.6621 -1)"
+                    fill="black"
+                />
+            </g>
+            <defs>
+                <clipPath id="clip0_1_2">
+                    <rect width="24" height="24" fill="white" />
+                </clipPath>
+            </defs>
+        </svg>
+    ),
+};
