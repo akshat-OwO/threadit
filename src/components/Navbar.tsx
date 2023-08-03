d1d88c270fcbc321b154d3fc6d11309241c707cd
Akshat Singh <akshatsinghdelhi@gmail.com>
2023-08-03 16:36:05 +0530

    user profile menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,34 @@
+import { getAuthSession } from '@/lib/auth';
 import Link from 'next/link';
 import { FC } from 'react';
 import { Icons } from './Icons';
+import UserAccountNav from './UserAccountNav';
 import { buttonVariants } from './ui/button';
 
 interface NavbarProps {}
 
-const Navbar: FC<NavbarProps> = ({}) => {
+const Navbar: FC<NavbarProps> = async ({}) => {
+    const session = await getAuthSession();
+
     return (
-        <div className='fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2'>
-            <div className='container max-w-7xl h-full mx-auto flex items-center justify-between gap-2'>
-                <Link href='/' className='flex gap-2 items-center'>
-                    <Icons.logo className='h-8 w-8 sm:h-6 sm:w-6' />
-                    <p className='hidden text-zinc-700 text-sm font-medium md:block'>ThreadIt</p>
+        <div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2">
+            <div className="container max-w-7xl h-full mx-auto flex items-center justify-between gap-2">
+                <Link href="/" className="flex gap-2 items-center">
+                    <Icons.logo className="h-8 w-8 sm:h-6 sm:w-6" />
+                    <p className="hidden text-zinc-700 text-sm font-medium md:block">
+                        ThreadIt
+                    </p>
                 </Link>
 
                 {/* search bar */}
-                
-                <Link href='/sign-in' className={buttonVariants()}>Sign In</Link>
+
+                {session?.user ? (
+                    <UserAccountNav user={session.user} />
+                ) : (
+                    <Link href="/sign-in" className={buttonVariants()}>
+                        Sign In
+                    </Link>
+                )}
             </div>
         </div>
     );
